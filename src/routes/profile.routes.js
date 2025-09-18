d38b26ceee2870ad6453320174c679544e7988df
Claudio Candia Vargas <claudio.candia2301@alumnos.ubiobio.cl>
2025-09-17 23:13:10 -0300

    add update and delete user routes

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -4,11 +4,17 @@ import {
   getPublicProfile,
   getPrivateProfile,
 } from "../controllers/profile.controller.js";
+import {
+  deleteUserById,
+  updateUserById,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get("/public", getPublicProfile);
 
 router.get("/private", authMiddleware, getPrivateProfile);
+router.patch("/private", authMiddleware, updateUserById);
+router.delete("/private", authMiddleware, deleteUserById);
 
 export default router;
