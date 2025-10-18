c371fe023457d4416cb791b1262ec4dc0f4260e5
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-10-18 01:43:43 -0300

    add edit and delete frontend services

--- frontend/src/services/profile.service.js
+++ frontend/src/services/profile.service.js
@@ -19,3 +19,43 @@ export async function getProfile() {
       }
     }
 }
+
+export async function updateProfile(newData) {
+  try {
+    const response = await axios.patch("/profile/private", newData);
+
+    return {
+      success: true,
+      data: response.data.data,
+      message: response.data.message
+    }
+  } catch (error) {
+    console.error("Error al actualizar perfil:", error);
+
+    return {
+      success: false,
+      message: error.response?.data?.message || "Error al actualizar perfil",
+      data: []
+    }
+  }
+}
+
+export async function deleteProfile() {
+  try {
+    const response = await axios.delete("/profile/private");
+    
+    return {
+      success: true,
+      data: response.data.data,
+      message: response.data.message
+    }
+  } catch (error) {
+    console.error("Error al eliminar perfil:", error);
+    
+    return {
+      success: false,
+      message: error.response?.data?.message || "Error al actualizar perfil",
+      data: []
+    }
+  }
+}
