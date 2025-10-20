ad362c0ad546e515bba2f5d1510a59ddbe83a4d4
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2025-10-20 00:24:21 -0300

    remove dash character from email regex

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -70,7 +70,7 @@ const Home = () => {
     setEditProfile(false);
   };
 
-  const emailRegex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{1,4}$/);
+  const emailRegex = new RegExp(/^[\w\.]+@([\w]+\.*)+[\w]{1,4}$/);
   const passwordRegex = new RegExp(/^(?=.*\d)(?=.*[.!@#$%&*_-])(?=.*[a-zñáéíóú])(?=.*[A-ZÑÁÉÍÓÚ]).{8,}$/);
   const handleConfirmEditProfile = async (e) => {
     e.preventDefault();
